d349273882e8b8761a883dc1e5ba2ac38eaea8b1
Masum Patel <masum.patel@compufytechnolab.com>
2023-02-05 15:43:17 +0530

    Added Basic outline for docker image

---
+++ DockerImage/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## TODO: 
+
+# 1. Clone the repo
+# 2. Start the model build pipeline as per input
+# 3. Take following input ( as Env Variable)
+#  - model-pipeline-version
+#  - DataSet Path
+#  - Path to push trained model
